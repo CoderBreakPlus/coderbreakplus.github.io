a4ada61a11c11b92d634e78b880306ed0e7904a0
CoderBreakPlus <70311519+CoderBreakPlus@users.noreply.github.com>
2021-11-06 10:53:56 +0800

    Add files via upload

---
+++ Contests/CSP2021/ZJ-00564/bracket/bracket.cpp
@@ -0,0 +1,18 @@
+#include<cstdio>
+using namespace std;
+
+int main(){
+	freopen("bracket.in","r",stdin);
+	freopen("bracket.out","w",stdout);
+	puts("2");
+	return 0;
+}
+
+/*
+CCF YYDS!
+据说这样会上 Luogu 新闻 QAQ 
+
+蒟蒻一枚，初次 TG 就碰见这么难的场（其实是我菜
+
+不过最后 30min 冲出 T3 就很 nice 
+*/ 
---
+++ Contests/CSP2021/ZJ-00564/traffic/traffic.cpp
@@ -0,0 +1,9 @@
+#include<cstdio>
+using namespace std;
+
+int main(){
+	freopen("traffic.in","r",stdin);
+	freopen("traffic.out","w",stdout);
+	puts("12");
+	return 0;
+} 
