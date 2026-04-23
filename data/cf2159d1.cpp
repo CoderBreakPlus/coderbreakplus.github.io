546cf93e4f29c1b347429efc1aaaaf3d904a6cb4
CoderBreakPlus <breakplus@foxmail.com>
2026-04-23 09:39:28 +0800

    批量更新

--- data/cf2159d1.cpp
+++ data/cf2159d1.cpp
@@ -1,27 +1,35 @@
 #include<bits/stdc++.h>
-#define F(i,a,b) for(long long i=(a);i<=(b);i++)
 using namespace std;
-#define ll long long
-const ll V=121,N=4e5+5,INF=2e18;
-ll T,n,a[N],stk[N],top,f[N][V],res;
-void solve(){
-	cin>>n,res=top=0;
-	F(i,0,n)F(j,0,V-1) f[i][j]=INF;
-	F(i,1,n) cin>>a[i];
-	F(r,1,n){
-		while(top&&a[stk[top]]>a[r]) top--;stk[++top]=r;
-		ll l[4]={-1,-1,-1,-1};
-		F(i,1,3){
-			ll L=1,R=top;
-			while(L<=R){
-				ll mid=(L+R)>>1;
-				if((a[r]-1)/a[stk[mid]]+1<=i) l[i]=mid,R=mid-1;
-				else L=mid+1;
+
+#define int long long
+#define MAXN 400005
+#define INF (int)2e18
+
+int n,a[MAXN],pnt[129];
+
+inline void solve(){
+	scanf("%lld",&n);
+	for( int i = 1 ; i <= n ; i ++ ) scanf("%lld",&a[i]);
+	int N = INF;
+	vector<int> A; A.emplace_back( -1 );
+	for( int i = n ; i >= 1 ; i -- ) if( a[i] < N ) A.emplace_back( a[i] ),N = a[i];
+	int siz = (int)A.size();
+	pnt[0] = 0; for( int i = 1 ; i <= 128 ; i ++ ) pnt[i] = -INF;
+	for( int i = 1 ; i < siz ; i ++ ){
+		for( int v = 128 ; v >= 0 ; v -- ){
+			if( pnt[v] > -INF ){
+				int newc = v + ( A[pnt[v] + 1] - 1 ) / A[i] + 1;
+				if( newc <= 128 ) pnt[newc] = max( pnt[newc] , i );
 			}
-			if(~l[i]) l[i]=stk[l[i]-1]+1;
 		}
-		F(i,0,V-2)F(j,1,3)if(~l[j]&&i+j<V) f[r][i+j]=min(f[r][i+j],i?f[l[j]-1][i]:l[j]);
-		F(i,1,V-1) f[r][i]=min(f[r][i],f[r][i-1]),res+=i*((i==1?r+1:f[r][i-1])-f[r][i]);
-    }cout<<res<<'\n';
+	}
+	int res = INF;
+	for( int i = 128 ; i > 0 ; i -- ) if( pnt[i] == siz - 1 ) res = i;
+	printf("%lld\n",res);
 }
-int main(){cin>>T;while(T--) solve();return 0;}
+
+signed main(){
+	int testcase; scanf("%lld",&testcase);
+	while( testcase -- ) solve();
+	return 0;
+}
