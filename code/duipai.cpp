c98ea69bdddca3bd2fd9931421df34a4cabb03a7
CoderBreakPlus <breakplus@foxmail.com>
2026-05-03 11:26:39 +0800

    批量更新

--- code/duipai.cpp
+++ code/duipai.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 int main(){
 	int T = 10000;
-	// cf1609f
-	assert(!system("g++ gen.cpp -o gen -DLOCAL"));
+	// soj2560
+	assert(!system("g++ gen.cpp -o gen"));
 	cerr<<"compiled gen"<<endl;
-	assert(!system("g++ brute.cpp -o brute -DLOCAL"));
+	assert(!system("g++ brute.cpp grader.cpp -o brute"));
 	cerr<<"compiled brute"<<endl;
-	assert(!system("g++ cf1609f.cpp -o cf1609f -DLOCAL"));
-	cerr<<"compiled cf1609f"<<endl;
+	assert(!system("g++ soj2560.cpp grader.cpp -o soj2560"));
+	cerr<<"compiled soj2560"<<endl;
 	for(int i=1;i<=T;i++){
-		assert(!system("./gen"));
+		assert(!system("./gen >test.in"));
 		// cerr<<"gen"<<endl;
-		assert(!system("./brute"));
+		assert(!system("./brute <test.in >test.out"));
 		// cerr<<"brute"<<endl;
-		assert(!system("./cf1609f"));
-		// cerr<<"cf1609f"<<endl;
+		assert(!system("./soj2560 <test.in >test.ans"));
+		// cerr<<"soj2560"<<endl;
 		if(system("diff -Zq test.out test.ans")){
 			cerr<<"WA on "<<i<<endl;
 			exit(0);
---
+++ code/tree.h
@@ -0,0 +1,10 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include <vector>
+
+void init(std::vector<int>p);
+double tree_hash(double a,double b);
+double solve(int x,int y,int l,int r);
+
+#endif // TREE_H
