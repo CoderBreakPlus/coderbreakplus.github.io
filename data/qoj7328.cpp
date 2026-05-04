4a9e7172147113014cc1c2dcb3bb652d7bacd9df
CoderBreakPlus <breakplus@foxmail.com>
2026-05-04 19:03:10 +0800

    批量更新

---
+++ data/qoj7328.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll,ll> P;
+#define fi first
+#define se second
+#define mkp make_pair
+#define pb emplace_back
+#define popcnt __builtin_popcountll
+const int mod = 998244353;
+inline ll read(){
+	ll x=0, f=1; int ch=getchar();
+	while(ch<'0' || ch>'9') { if(ch=='-') f=-1; ch=getchar(); }
+	while(ch>='0' && ch<='9') x=x*10+ch-'0', ch=getchar();
+	return x*f;
+}
+inline int lg2(int x){ return 31^__builtin_clz(x); }
+inline ll lg2(ll x){ return 63^__builtin_clzll(x); }
+inline void addmod(int &x){ if(x >= mod) x -= mod; }
+inline void addmod(ll &x){ if(x >= mod) x -= mod; }
+inline ll qpow(ll a,ll b){
+	ll ans=1, base=a;
+	while(b){
+		if(b&1) ans=ans*base%mod;
+		base=base*base%mod; b>>=1;
+	}
+	return ans;
+}
+inline ll INV(ll x){ return qpow(x, mod-2); }
+
+int n,a[32];
+mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+ll rng(ll x,ll y){ return x+rnd()%(y-x+1); }
+
+void procedure(){
+	n=read();
+	for(int i=1;i<=n;i++)a[i]=read();
+
+	ll ans=0;
+	while(a[1]){
+		int x=0;
+		while(a[x+1]==x+1) x++;
+
+		if(x){
+			ans+=(1ll<<x-1)-1;
+			for(int i=x;i>=2;i--) a[i]=a[i-1];
+			a[1]=x;
+		}
+
+		x=a[1];
+		for(int i=1;i<=x;i++)a[i]=a[i+1];
+		a[x+1]=x;
+		ans++;
+
+		// for(int i=1;i<=n;i++)cout<<a[i]<<" ";cout<<endl;
+	}
+	printf("%lld\n",ans);
+}
+int main(){
+	#ifdef LOCAL
+		assert(freopen("test.in","r",stdin));
+		assert(freopen("test.out","w",stdout));
+	#endif
+	ll T=1;
+	// math_init();
+	// NTT::init();
+	while(T--) procedure();
+	return 0;
+}
