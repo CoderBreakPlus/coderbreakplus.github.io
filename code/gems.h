77603e39ad79a87ad63e773acbe761beec8ac49b
CoderBreakPlus <breakplus@foxmail.com>
2026-05-14 16:53:08 +0800

    批量更新

--- code/gems.h
+++
@@ -1,9 +0,0 @@
-#ifndef GEMS_H
-#define GEMS_H
-
-#include <vector>
-
-void gems(int c, int n, int m, std::vector<int> u, std::vector<int> v, std::vector<int> a, std::vector<int> d);
-long long query(int x, int l, int r);
-
-#endif // GEMS_H
