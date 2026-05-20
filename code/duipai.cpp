15013a3e300c80a0cae3d92045a09a8c1d112b6c
CoderBreakPlus <breakplus@foxmail.com>
2026-05-20 11:00:54 +0800

    批量更新

--- code/duipai.cpp
+++ code/duipai.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 int main(){
 	int T = 10000;
-	// B
+	// cf1332g
 	assert(!system("g++ gen.cpp -o gen -DLOCAL"));
 	cerr<<"compiled gen"<<endl;
 	assert(!system("g++ brute.cpp -o brute -DLOCAL"));
 	cerr<<"compiled brute"<<endl;
-	assert(!system("g++ B.cpp -o B -DLOCAL"));
-	cerr<<"compiled B"<<endl;
+	assert(!system("g++ cf1332g.cpp -o cf1332g -DLOCAL"));
+	cerr<<"compiled cf1332g"<<endl;
 	for(int i=1;i<=T;i++){
 		assert(!system("./gen"));
 		cerr<<"gen"<<endl;
 		assert(!system("./brute"));
 		cerr<<"brute"<<endl;
-		assert(!system("./B"));
-		cerr<<"B"<<endl;
+		assert(!system("./cf1332g"));
+		cerr<<"cf1332g"<<endl;
 		if(system("diff -Zq test.out test.ans")){
 			cerr<<"WA on "<<i<<endl;
 			exit(0);
