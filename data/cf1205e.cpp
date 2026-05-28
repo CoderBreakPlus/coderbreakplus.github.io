916ce0cf3ee48a4c6997fdc9a5d0ec8c3c0a829d
CoderBreakPlus <breakplus@foxmail.com>
2026-05-28 20:19:06 +0800

    批量更新

---
+++ data/cf1205e.cpp
@@ -0,0 +1,80 @@
+// border length: i, j
+// let x=n-i, y=n-j
+// number of connected components: max(gcd(x,y), x+y-n)
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const int mod = 1e9 + 7;
+
+inline ll qpow(ll a, ll b) {
+    ll ans = 1, base = a;
+    while (b) {
+        if (b & 1) ans = ans * base % mod;
+        base = base * base % mod; b >>= 1;
+    }
+    return ans;
+}
+inline ll INV(ll x) { return qpow(x, mod - 2); }
+
+int n, k;
+vector<int> pr[200005];
+
+void pre_primes(int limit) {
+    for (int i = 2; i <= limit; i++) {
+        if (pr[i].empty()) {
+            for (int j = i; j <= limit; j += i) {
+                pr[j].push_back(i);
+            }
+        }
+    }
+}
+
+int count_coprime(int s, int R) {
+    if (R <= 0) return 0;
+    int res = 0;
+    int k_sz = pr[s].size();
+    for (int mask = 0; mask < (1 << k_sz); mask++) {
+        int p = 1, sign = 1;
+        for (int i = 0; i < k_sz; i++) {
+            if ((mask >> i) & 1) {
+                p *= pr[s][i];
+                sign = -sign;
+            }
+        }
+        res += sign * (R / p);
+    }
+    return res;
+}
+
+void procedure() {
+    cin >> n >> k;
+    pre_primes(2 * n);
+    ll ans = 0;
+    for (int d = 1; d < n; d++) {
+        int M = (n - 1) / d;
+        for (int s = 2; s <= 2 * M; s++) {
+            int L = max(1, s - M);
+            int R = min(s - 1, M);
+            if (L > R) continue;
+            int cnt = count_coprime(s, R) - count_coprime(s, L - 1);
+            if (!cnt) continue;
+            int c = max(d, s * d - n);
+            ans = (ans + 1LL * cnt * qpow(k, c)) % mod;
+        }
+    }
+    ans = ans * qpow(INV(k), n) % mod;
+    cout << ans << "\n";
+}
+
+int main() {
+    #ifdef LOCAL
+        assert(freopen("test.in","r",stdin));
+        assert(freopen("test.out","w",stdout));
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    procedure();
+    return 0;
+}
---
+++ data/cf2229h.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IL inline
+const int N = 3010, mod = 998244353;
+IL int add(int x, int y) { return x + y < mod ? x + y : x + y - mod; }
+#define t(i) (s[i] & 1)
+int n, s[N], lst[N], to[N][2][2][2], nxt[N], __[N][N]; string S;
+IL int z(int u, int v, int p, int q) {
+  int Q = nxt[min(u, v) + 1];
+  return min(to[min(Q, u) + 1][p][q][0], to[min(Q, v) + 1][p][q][1]);
+}
+IL int F(int u, int v) {
+  int &ans = __[u + 1][v + 1];
+  return u >= n && v >= n ? 0 : ~ans ? ans : ans = add((u < n && (!t(n) || u < lst[n - 1])) || (v < n && (t(n) || v < lst[n - 1])), add(F(z(u, v, 0, 0), z(u, v, 0, 1)), F(z(u, v, 1, 0), z(u, v, 1, 1))));
+}
+void ___() {
+  cin >> n >> S, nxt[n] = nxt[n + 1] = n;
+  for (int i = 0; i < n; ++i) s[i + 1] = s[i] + (S[i] == '1');
+  for (int i = 0; i < n; ++i) lst[i] = S[i] == '?' ? i : (!i ? -1 : lst[i - 1]);
+  for (int i = n - 1; ~i; --i) nxt[i] = S[i] == '?' ? i : nxt[i + 1];
+  for (int p : {0, 1}) for (int q : {0, 1}) for (int v : {0, 1})
+    to[n][p][q][v] = to[n + 1][p][q][v] = n;
+  for (int i = n - 1; ~i; --i) {
+    memcpy(to[i], to[i + 1], sizeof to[i]);
+    for (int p : {0, 1}) if (S[i] == '?' || S[i] == p + '0') to[i][p][t(i + 1)][t(i)] = i;
+  }
+  for (int i = 0; i <= n + 5; ++i) for (int j = 0; j <= n + 5; ++j) __[i][j] = -1;
+  cout << F(-1, n) << '\n';
+}
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int _; cin >> _; while (_--) ___();
+}
