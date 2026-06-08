6365512b929833e43405bafefb5d6d1f1b7c8a7e
CoderBreakPlus <breakplus@foxmail.com>
2026-06-09 07:37:36 +0800

    批量更新

--- data/qoj14419.cpp
+++ data/qoj14419.cpp
@@ -1,26 +1,33 @@
-// created time: 2026-05-14 08:07:14
-// https://qoj.ac/contest/2559/problem/14419
+// created time: 2026-06-08
+// 晚上和同学出去吃火锅。然后被 fsb 发现我们趁高考没人管跑路了。
+// 回家真的没有事情能干。我妈就坐我后面。
+// 咬牙，再熬两年就自由啦。
+
+// 本来打算摆烂，遇到这样一个好题实在是让人心旷神怡。
+// 仿佛回到了初一的时候，看到的每一个新奇的思想和套路，都为之动容。
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
+typedef pair<ll,ll> P;
 #define fi first
 #define se second
 #define mkp make_pair
 #define pb emplace_back
 #define popcnt __builtin_popcountll
-const int mod = 998244353;
+const ll mod = 998244353;
 inline ll read(){
-	ll x=0, f=1; int ch=getchar();
+	ll x=0, f=1; char ch=getchar();
 	while(ch<'0' || ch>'9') { if(ch=='-') f=-1; ch=getchar(); }
 	while(ch>='0' && ch<='9') x=x*10+ch-'0', ch=getchar();
 	return x*f;
 }
 inline int lg2(int x){ return 31^__builtin_clz(x); }
 inline ll lg2(ll x){ return 63^__builtin_clzll(x); }
-template<typename T>inline void addmod(T &x){ if(x >= mod) x -= mod; }
-template<typename T>inline void chkmax(T &a,T b){ a=max(a,b); }
-template<typename T>inline void chkmin(T &a,T b){ a=min(a,b); }
+inline void addmod(int &x){ if(x >= mod) x -= mod; }
+inline void addmod(ll &x){ if(x >= mod) x -= mod; }
+template<typename T>void chkmax(T &a,T b){ a=max(a,b); }
+template<typename T>void chkmin(T &a,T b){ a=min(a,b); }
 inline ll qpow(ll a,ll b){
 	ll ans=1, base=a;
 	while(b){
@@ -29,63 +36,80 @@ inline ll qpow(ll a,ll b){
 	}
 	return ans;
 }
-inline ll INV(ll x){ return qpow(x, mod-2); }
+inline ll INV(ll x){ return qpow(x, mod-2); };
+const int N = 500000;
+int fac[N+5],inv[N+5];
+void math_init(){
+	fac[0]=inv[0]=1;
+	for(int i=1;i<=N;i++) fac[i]=1ll*fac[i-1]*i%mod;
+	inv[N]=qpow(fac[N],mod-2);
+	for(int i=N-1;i>=1;i--) inv[i]=1ll*inv[i+1]*(i+1)%mod;
+}
+inline int binom(int x,int y){
+	if(x<0 || y<0 || x<y) return 0;
+	return 1ll*fac[x]*inv[y]%mod*inv[x-y]%mod;
+}
+inline int perm(int x,int y){
+	if(x<0 || y<0 || x<y) return 0;
+	return 1ll*fac[x]*inv[x-y]%mod;
+}
 
-int n,m,q,s[1000005],t[1000005],u[1000005],v[1000005],tim[1000005];
-int fa[1000005];
-unordered_map<int,int>mp[1000005];
-
-inline int find(int x){ if(x!=fa[x])fa[x]=find(fa[x]);return fa[x]; }
-inline void merge(int x,int y){ fa[find(x)]=find(y); }
-void procedure(){
-	n=read(),m=read(),q=read();
-	for(int i=0;i<n;i++)mp[i].clear();
-	for(int i=1;i<=m;i++){
-		s[i]=read(),t[i]=read();
-		mp[s[i]][t[i]]=mp[t[i]][s[i]]=i;
-		tim[i]=q+1;
-	}
-
-	int v2=0,v3=0;
-	for(int i=1;i<=q;i++){
-		u[i]=(read()+v2)%n,v[i]=(read()+v3)%n;
-		if(mp[u[i]].count(v[i])){
-			chkmin(tim[mp[u[i]][v[i]]],i);
+int n,qz[500005];
+int brute(int n,int L,int R){
+	if(L>R)return 0;
+	return (qz[R]-(L?qz[L-1]:0)+mod)%mod;
+}
+int calc2(int a,int b,int n){
+	int l=b+1,r=a-1;
+	int ans=brute(n,(n+l+1)>>1,(n+r)>>1);
+	auto fold = [&](int x){
+		l=2*x-l,r=2*x-r;swap(l,r);
+		chkmax(l,-n),chkmin(r,n);
+	};
+	for(int x=1;l<=r;x++){
+		if(x&1){
+			fold(a);
+			addmod(ans+=mod-brute(n,(n+l+1)>>1,(n+r)>>1));
+		}else{
+			fold(b);
+			addmod(ans+=brute(n,(n+l+1)>>1,(n+r)>>1));
 		}
-		v2=(v2*2+1)%n, v3=(v3*3+1)%n;
 	}
-
-	int L=1,R=q,ans=0;
-	while(L<=R){
-		int mid=(L+R)>>1;
-		for(int i=0;i<n;i++)fa[i]=i;
-		for(int i=1;i<=m;i++)
-			if(tim[i]>mid){
-				merge(s[i],t[i]);
-			}
-
-		bool ok=1;
-		for(int i=1;i<n;i++)
-			ok&=(find(i)==find(0));
-
-		if(ok){
-			ans=mid;
-			L=mid+1;
-		}else R=mid-1;
+	l=b+1,r=a-1;
+	for(int x=1;l<=r;x++){
+		if(x&1){
+			fold(b);
+			addmod(ans+=mod-brute(n,(n+l+1)>>1,(n+r)>>1));
+		}else{
+			fold(a);
+			addmod(ans+=brute(n,(n+l+1)>>1,(n+r)>>1));
+		}
 	}
-
-	for(int i=1;i<=q;i++){
-		putchar((i<=ans)+'0');
-		putchar('\n');
+	return ans;
+}
+int out[500005];
+void procedure(){
+	n=read();
+	for(int k=0;k<=n;k++){
+		if(k)qz[k]=qz[k-1];
+		addmod(qz[k]+=binom(n,k));
 	}
+	for(int k=0;k<=n;k++){
+		int a=k+1,b=-k-2;
+		out[k]=calc2(a,b,n);
+	}
+	for(int i=n;i>=1;i--)addmod(out[i]+=mod-out[i-1]);
+	for(int i=0;i<=n;i++)printf("%d ",out[i]);
+	puts("");
 }
 int main(){
 	#ifdef LOCAL
 		assert(freopen("test.in","r",stdin));
 		assert(freopen("test.out","w",stdout));
 	#endif
-	ll T=read();
-	// math_init();
+	ll T=1;
+	math_init();
+	// NTT::init();
 	while(T--) procedure();
 	return 0;
 }
