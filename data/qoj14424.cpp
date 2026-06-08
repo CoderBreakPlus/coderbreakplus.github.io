089432ee68f8785515c51f3521899f9aa535f5df
CoderBreakPlus <breakplus@foxmail.com>
2026-06-08 09:00:06 +0800

    批量更新

--- data/qoj14424.cpp
+++ data/qoj14424.cpp
@@ -1,5 +1,5 @@
-// created time: 2026-05-14 07:19:06
-// https://qoj.ac/contest/2559/problem/14426
+// created time: 2026-05-14 07:23:15
+// https://qoj.ac/contest/2559/problem/14425
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -30,27 +30,71 @@ inline ll qpow(ll a,ll b){
 	return ans;
 }
 inline ll INV(ll x){ return qpow(x, mod-2); }
+#define pii pair<int,int>
+int n,a[100005],ans[100005];
 
-int n,a[100005];
+pii c[100005],dp[100005];
+
+void upd(int x,pii w){
+	while(x<=n){
+		chkmax(c[x],w);
+		x+=(x&-x);
+	}
+}
+pii qry(int x){
+	pii res={0,0};
+	while(x){
+		chkmax(res,c[x]);
+		x-=(x&-x);
+	}
+	return res;
+}
+pii calc(int v){
+	for(int i=1;i<=n;i++)c[i]={0,0};
+
+	pii tmp={0,0};
+	for(int i=1;i<=n;i++){
+		pii cur=qry(a[i]-1);
+		dp[i]=(pii){cur.fi+1,cur.se};
+		if(i>1) chkmax(dp[i], (pii){tmp.fi-v+1,tmp.se+1});
+		upd(a[i],dp[i]), chkmax(tmp,dp[i]);
+	}
+	return {tmp.fi-v,tmp.se+1};
+}
+void solve(int l,int r,int cl,int cr){
+	if(l+1>=r)return;
+	if(cl==cr){
+		for(int i=l;i<=r;i++)ans[i]=cl;
+		return;
+	}
+	int mid=(l+r)>>1;
+	ans[mid]=calc(mid).se;
+	solve(l,mid,cl,ans[mid]);
+	solve(mid,r,ans[mid],cr);
+}
 
 void procedure(){
 	n=read();
-	int d=0;
 	for(int i=1;i<=n;i++)a[i]=read();
-	sort(a+1,a+n+1);
-	
-	for(int i=1;i<=n;i++)d=__gcd(d,a[i]);
-	
-	int cnt=a[n]/d-n;
-	if(cnt%3==0)puts("YES");
-	else puts("NO");
+	solve(0,n,ans[0]=calc(0).se,ans[n]=calc(n).se);
+
+	// for(int i=0;i<=n;i++)cout<<ans[i]<<" ";cout<<endl;
+	int pt=n, val=calc(n).fi;
+	for(int i=1;i<=n;i++){
+		if(pt && ans[pt]<i){
+			while(pt && ans[pt]<i) pt--;
+			val=calc(pt).fi;
+		}
+		printf("%d ",val+pt*i);
+	}
+	puts("");
 }
 int main(){
 	#ifdef LOCAL
 		assert(freopen("test.in","r",stdin));
 		assert(freopen("test.out","w",stdout));
 	#endif
-	ll T=1;
+	ll T=read();
 	// math_init();
 	while(T--) procedure();
 	return 0;
--- data/qoj14426.cpp
+++ data/qoj14426.cpp
@@ -1,5 +1,5 @@
-// created time: 2026-05-14 07:19:06
-// https://qoj.ac/contest/2559/problem/14426
+// created time: 2026-06-08 08:34:36
+// 单位根反演
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -9,7 +9,7 @@ typedef unsigned long long ull;
 #define mkp make_pair
 #define pb emplace_back
 #define popcnt __builtin_popcountll
-const int mod = 998244353;
+const int mod = 1e9+9, w[3] = {1,115381398,884618610};
 inline ll read(){
 	ll x=0, f=1; int ch=getchar();
 	while(ch<'0' || ch>'9') { if(ch=='-') f=-1; ch=getchar(); }
@@ -30,20 +30,49 @@ inline ll qpow(ll a,ll b){
 	return ans;
 }
 inline ll INV(ll x){ return qpow(x, mod-2); }
+const int N = 500000;
+int fac[N+5],inv[N+5];
+void math_init(){
+	fac[0]=inv[0]=1;
+	for(int i=1;i<=N;i++) fac[i]=1ll*fac[i-1]*i%mod;
+	inv[N]=qpow(fac[N],mod-2);
+	for(int i=N-1;i>=1;i--) inv[i]=1ll*inv[i+1]*(i+1)%mod;
+}
+inline int binom(int x,int y){
+	if(x<0 || y<0 || x<y) return 0;
+	return 1ll*fac[x]*inv[y]%mod*inv[x-y]%mod;
+}
+inline int perm(int x,int y){
+	if(x<0 || y<0 || x<y) return 0;
+	return 1ll*fac[x]*inv[x-y]%mod;
+}
 
-int n,a[100005];
+int h,m,k,f[3][3];
 
 void procedure(){
-	n=read();
-	int d=0;
-	for(int i=1;i<=n;i++)a[i]=read();
-	sort(a+1,a+n+1);
-	
-	for(int i=1;i<=n;i++)d=__gcd(d,a[i]);
-	
-	int cnt=a[n]/d-n;
-	if(cnt%3==0)puts("YES");
-	else puts("NO");
+	h=read(),m=read(),k=read()+1;
+
+	for(int i:{0,1,2})for(int j:{0,1,2}){
+		if((i+j)%3==0){
+			f[i][j]=k;
+			continue;
+		}
+		for(int t=0;t<k%3;t++){
+			addmod(f[i][j]+=w[(i+j)*t%3]);
+		}
+	}
+
+	int ans=0;
+	for(int h0=0;h0<=h;h0++){
+		for(int h1=0;h0+h1<=h;h1++){
+			int h2=h-h0-h1;
+			int cf=(ull)fac[h]*inv[h0]%mod*inv[h1]%mod*inv[h2]%mod,val=0;
+			for(int w:{0,1,2})
+				val=(val+qpow(f[0][w],h0)*qpow(f[1][w],h1)%mod*qpow(f[2][w],h2))%mod;
+			ans=(ans+(ull)cf*qpow(val,m))%mod;
+		}
+	}
+	printf("%llu\n",(ull)ans*qpow(INV(3),h+m)%mod);
 }
 int main(){
 	#ifdef LOCAL
@@ -51,7 +80,7 @@ int main(){
 		assert(freopen("test.out","w",stdout));
 	#endif
 	ll T=1;
-	// math_init();
+	math_init();
 	while(T--) procedure();
 	return 0;
 }
