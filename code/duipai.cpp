4c405a7e589da83a7f644a2ae36964bf7bd2e120
CoderBreakPlus <breakplus@foxmail.com>
2026-06-17 07:08:45 +0800

    批量更新

--- code/duipai.cpp
+++ code/duipai.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 int main(){
 	int T = 1000;
-	assert(!system("g++ I.cpp -o I -DLOCAL"));
-	assert(!system("g++ brute.cpp -o brute -DLOCAL"));
+	assert(!system("g++ C.cpp -o C -DLOCAL"));
+	assert(!system("g++ bruteC.cpp -o bruteC -DLOCAL"));
 	assert(!system("g++ gen.cpp -o gen -DLOCAL"));
 	cout<<"Compiled"<<endl;
 	for(int i=1;i<=T;i++){
 		assert(!system("./gen"));
-		assert(!system("./brute"));
-		assert(!system("./I"));
+		assert(!system("./bruteC"));
+		assert(!system("./C"));
 		if(system("diff -Zq test.out test.ans")){
 			cout<<"WA on "<<i<<endl;
 			exit(0);
