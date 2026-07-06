08b1498791f70da0b2e380929efc260d693b0b95
CoderBreakPlus <breakplus@foxmail.com>
2026-07-06 14:06:15 +0800

    批量更新

--- data/p9499.cpp
+++ data/p9499.cpp
@@ -1,4 +1,4 @@
-// created time: 2026-07-01 08:27:37
+// created time: 2026-07-02 08:18:12
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -8,7 +8,7 @@ typedef unsigned long long ull;
 #define mkp make_pair
 #define pb emplace_back
 #define popcnt __builtin_popcountll
-const int mod = 1e9+7;
+const int mod = 998244353;
 inline ll read(){
 	ll x=0, f=1; int ch=getchar();
 	while(ch<'0' || ch>'9') { if(ch=='-') f=-1; ch=getchar(); }
@@ -30,51 +30,60 @@ inline ll qpow(ll a,ll b){
 }
 inline ll INV(ll x){ return qpow(x, mod-2); }
 
-char s[10005];
-int n;
+ll n,v[200005],c[200005],x[200005];
 
-int dp[10005][10005],cf;
-
-void solve(){
-	for(int i=0;i<=n;i++)for(int j=0;j<=n;j++)
-		dp[i][j]=0;
-
-	dp[0][0]=1;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<=i;j++){
-			addmod(dp[i+1][j+1]+=dp[i][j]);
-			if(s[i]=='0')
-				dp[i+1][j]=(dp[i+1][j]+(ull)dp[i][j]*j)%mod;
-		}
-	}
-	int ans=0;
-	for(int i=0;i<=n;i++)
-		addmod(ans+=dp[n][i]);
-	ans=(ull)ans*cf%mod;
-	printf("%d\n",ans);
-}
 void procedure(){
-	scanf("%s",s);n=strlen(s);
-	for(int i=0;i<n;i++)cf+=(s[i]=='0');
-	cf=qpow((mod+1)/2,cf);
-	solve();
+	n=read();
+	for(int i=1;i<=n;i++)v[i]=read();
+	for(int i=1;i<=n;i++)c[i]=read();
+	for(int i=1;i<n;i++)x[i]=read();
 
-	int q=read();
-	while(q--){
-		int i=read();swap(s[i-1],s[i]);
-		solve();
+	int ans=0;
+	multiset<pair<ll,ll>>S;
+	for(int i=1;i<=n;i++){
+		while(!S.empty()&&S.begin()->fi<v[i]){
+			ans=(ans+(ll)(2*mod-S.begin()->fi)*((S.begin()->se)%mod))%mod;
+			c[i]+=S.begin()->se;
+			S.erase(S.begin());
+		}
+		ans=(ans+(ll)v[i]*(c[i]%mod))%mod; S.emplace(v[i],c[i]);
+		if(i==n)break;
+
+		if(x[i]==1)continue;
+		multiset<pair<ll,ll>>T;
+		while(!S.empty()){
+			int t=0; ll sum=0;
+			while(t<x[i]&&!S.empty()){
+				auto [V,C]=*S.begin();S.erase(S.begin());
+
+				if(t+C<=x[i]){
+					sum+=V*C;
+					t+=C;
+				}else{
+					ll d=x[i]-t;
+					sum+=V*d,t+=d; C-=d;
+
+					if(C>x[i]){
+						if(V*x[i]<=1e9)T.emplace(V*x[i],C/x[i]);
+						C%=x[i];
+					}
+					if(C)S.emplace(V,C);
+				}
+			}
+			if(sum>1e9||t<x[i])break;
+			T.emplace(sum,1);
+		}
+		swap(S,T);
 	}
+	printf("%d\n",ans);
 }
 int main(){
 	#ifdef LOCAL
 		assert(freopen("test.in","r",stdin));
 		assert(freopen("test.out","w",stdout));
 	#endif
-	#ifndef LOCAL
-		assert(freopen("pow.in","r",stdin));
-		assert(freopen("pow.out","w",stdout));
-	#endif
-	ll T=1;
+	read();
+	ll T=read();
 	// math_init();
 	while(T--) procedure();
 	return 0;
