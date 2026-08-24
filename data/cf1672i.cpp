2064b3122fc379df7cca5769ae40a5b7f0fd5c6c
Zijun Wang <breakplus@foxmail.com>
2026-08-24 22:40:40 +0800

    批量更新

--- data/cf1672i.cpp
+++ data/cf1672i.cpp
@@ -1,4 +1,4 @@
-// created time: 2026-08-24 10:40:09
+// created time: 2026-08-24
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -29,14 +29,9 @@ inline ll qpow(ll a,ll b){
 	return ans;
 }
 inline ll INV(ll x){ return qpow(x, mod-2); }
-
-const int M = (1<<20)+5;
-const int N = 500005;
-
-int n,vis[N],p[N];
-int ta,tb,ans;
-pair<int,int> a[N],b[N];
-
+const int N=5e5+5,INF=1e9;
+int n,ans,p[N];
+int ABS(int x){ return x<0?-x:x; }
 struct BIT{
 	int c[N];
 	void upd(int x,int w){
@@ -53,119 +48,154 @@ struct BIT{
 		}
 		return ret;
 	}
-	void clear(){ for(int i=1;i<=n;i++) c[i]=0; }
 }bx,by;
 
-int vis[N];
-namespace L{
-	struct S0{
-		int mn[M],m;
-		void pushup(int p){ mn[p]=min(mn[p<<1],mn[p<<1|1]); }
-		void init(){
-			m=1;
-			while(m<=n+1)m<<=1;
-			for(int i=1;i<=2*m;i++)mn[i]=1e9;
-		}
-		void upd(int x,int v){
-			mn[x+=m]=v;
-			for(x>>=1;x;x>>=1)pushup(x);
-		}
-		void findr(int x,int v){
-
-		}
-	}s0;
-	struct S1{
-		int mn[M],val[M],id[M],tag[M];
-		void pushup(int p){
-			if(val[p<<1]<val[p<<1|1])
-				val[p]=val[p<<1],id[p]=id[p<<1];
-			else
-				val[p]=val[p<<1|1],id[p]=id[p<<1|1];
-		}
-		void upd(int p,int v){ val[p]+=v,tag[p]+=v; }
-		void pushdown(int p){ if(tag[p]) upd(p<<1,tag[p]),upd(p<<1|1,tag[p]),tag[p]=0; }
-		void build(int l,int r,int p){
-			tag[p]=0;
-			if(l==r){
-				val[p]=mn[p]=1e9;
-				return;
-			}
-			int mid=(l+r)>>1;
-			build(l,mid,p<<1),build(mid+1,r,p<<1|1);
-			pushup(p);
-		}
-		void modify(int l,int r,int x,int op,int p){
-			if(l==r){
-				if(op) mn[p]=by.qry(p[l]),val[p]=mn[p]-bx.qry(l);
-				else val[p]=mn[p]=1e9;
-				return;
-			}
-			int mid=(l+r)>>1;pushdown(p);
-			if(x<=mid)modify(l,mid,x,op,p<<1);
-			else modify(mid+1,r,x,op,p<<1|1);
-			pushup(p);
-		}
-		void update(int l,int r,int ql,int qr,int v,int p){
-			if(r<ql||qr<l) return;
-			if(ql<=l && r<=qr){ upd(p,v);return; }
-			int mid=(l+r)>>1;pushdown(p);
-			update(l,mid,ql,qr,v,p<<1);
-			update(mid+1,r,ql,qr,v,p<<1|1);
-			pushup(p);
-		}
-		void update2(int l,int r,int ql,int qr,int v,int p){
-
-		}
-	}s1;
-	void init(){
-		int cur=1e9;
-		s0.init(),s1.build(1,n,1);
-		for(int i=1;i<=ta;i++){
-			auto [x,y]=a[i];
-			if(y<cur){
-				vis[i]=1,cur=y;
-				s1.modify(1,n,x,1,1);
-			}else{
-				s0.upd(a[i].fi,a[i].se);
-			}
-		}
+struct Seg{
+	bool op;
+	int val[N*4],mn[N*4],mx[N*4],id[N*4],tag[N*4];
+	void upd(int p,int w){
+		val[p]+=w,tag[p]+=w;
 	}
-	void del(int i){
-		s1.modify(1,n,i,0,1);
-		// pi - i
-		// for every j >= i, val ++
-		s1.update(1,n,i,n,1,1);
-
+	void pushup(int p){
+		val[p]=min(val[p<<1],val[p<<1|1]);
+		mn[p]=min(mn[p<<1],mn[p<<1|1]);
+		mx[p]=max(mx[p<<1],mx[p<<1|1]);
+		id[p]=id[p<<1|(val[p]==val[p<<1|1])];
 	}
-}
-
-void del(int i){
-	int x=bx.qry(i),y=by.qry(p[i]);
-	bx.upd(i,-1),by.qry(p[i],-1);
-	chkmax(ans, max(x-y,y-x));
-	
-	vis[i]=2;
-	if(i<=p[i]) L::del(i);
-	else R::del(i);
-}
+	void pushdown(int p){
+		if(tag[p])
+			upd(p<<1,tag[p]),upd(p<<1|1,tag[p]),tag[p]=0;
+	}
+	void modify(int l,int r,int ql,int qr,int v,int p){
+		if(r<ql||qr<l)return;
+		if(ql<=l&&r<=qr){ upd(p,v); return; }
+		int mid=(l+r)>>1; pushdown(p);
+		modify(l,mid,ql,qr,v,p<<1);
+		modify(mid+1,r,ql,qr,v,p<<1|1);
+		pushup(p);
+	}
+	void update(int l,int r,int x,int a,int b,int c,int p){
+		if(l==r){
+			val[p]=a,mn[p]=b,mx[p]=c;
+			return;
+		}
+		int mid=(l+r)>>1; pushdown(p);
+		if(x<=mid)update(l,mid,x,a,b,c,p<<1);
+		else update(mid+1,r,x,a,b,c,p<<1|1);
+		pushup(p);
+	}
+	int active(int l,int r,int ql,int qr,int cur,int p){
+		if(r<ql||qr<l||cur<=mn[p])return cur;
+		if(l==r){
+			int pos=op?n-l+1:l;
+			cur=mn[p];
+			mx[p]=mn[p],mn[p]=INF;
+			val[p]=ABS(bx.qry(pos)-by.qry(::p[pos]));
+			return cur;
+		}
+		int mid=(l+r)>>1;pushdown(p);
+		cur=active(mid+1,r,ql,qr,cur,p<<1|1);
+		cur=active(l,mid,ql,qr,cur,p<<1);
+		pushup(p);
+		return cur;
+	}
+	int qry(int l,int r,int v,int p){
+		if(mx[p]<v)return n+1; 
+		if(l==r) return l;
+		int mid=(l+r)>>1;
+		if(mx[p<<1]>=v) return qry(l,mid,v,p<<1);
+		else return qry(mid+1,r,v,p<<1|1);
+	}
+	int find(int l,int r,int x,int p){
+		if(mx[p]<0) return 0;
+		if(l==r) return l;
+		int mid=(l+r)>>1;
+		if(x>mid){
+			int ret=find(mid+1,r,x,p<<1|1);
+			if(ret)return ret;
+		}
+		return find(l,mid,x,p<<1);
+	}
+	int find_right(int l,int r,int x,int p){
+	    if(mx[p]<0) return n+1;
+	    if(l==r) return l;
+	    int mid=(l+r)>>1;
+	    if(x<=mid){
+	        int ret=find_right(l,mid,x,p<<1);
+	        if(ret<=n)return ret;
+	    }
+	    return find_right(mid+1,r,x,p<<1|1);
+	}
+	void build(int l,int r,int p){
+		tag[p]=0;
+		if(l==r){
+			val[p]=INF,mn[p]=INF,mx[p]=-INF,id[p]=l;
+			return;
+		}
+		int mid=(l+r)>>1;
+		build(l,mid,p<<1),build(mid+1,r,p<<1|1);
+		pushup(p);
+	} 
+}T1,T2;
 void procedure(){
 	n=read();
-	ans=ta=tb=0;
-	for(int i=1;i<=n;i++){
-		p[i]=read(),vis[i]=0;
-		if(i<=p[i]) a[++ta]={i,p[i]};
-		else b[++tb]={i,p[i]};
-	}
-	reverse(a+1,a+ta+1);
-	L::init();
+	for(int i=1;i<=n;i++)p[i]=read();
+	for(int i=1;i<=n;i++)bx.upd(i,1),by.upd(i,1);
 
+	T2.op=1;
+	T1.build(1,n,1),T2.build(1,n,1);
+	for(int i=1;i<=n;i++){
+		if(i<p[i])T1.update(1,n,i,INF,p[i],-INF,1);
+		else T2.update(1,n,n-i+1,INF,n-p[i]+1,-INF,1);
+	}
+	T1.active(1,n,1,n,INF,1);
+	T2.active(1,n,1,n,INF,1);
+
+	for(int i=1;i<=n;i++){
+		if(T1.val[1]<T2.val[1]){
+			chkmax(ans,T1.val[1]);
+			int pos=T1.id[1];
+
+			T1.update(1,n,pos,INF,INF,-INF,1);
+			bx.upd(pos,-1),by.upd(p[pos],-1);
+
+			int tmp=T1.qry(1,n,p[pos],1);
+			T1.modify(1,n,tmp,pos,-1,1);
+
+			tmp=T2.qry(1,n,n-p[pos]+1,1);
+			T2.modify(1,n,tmp,n-pos+1,-1,1);
+
+			tmp=T1.find(1,n,pos,1);
+			int tmp2=T1.find_right(1,n,pos,1);
+			T1.active(1,n,tmp+1,pos-1,(tmp2<=n)?p[tmp2]:INF,1);
+		}else{
+			chkmax(ans,T2.val[1]);
+			int pos=T2.id[1];
+
+			T2.update(1,n,pos,INF,INF,-INF,1);
+			pos=n-pos+1;
+
+			bx.upd(pos,-1),by.upd(p[pos],-1);
+
+			int tmp=T1.qry(1,n,p[pos],1);
+			T1.modify(1,n,tmp,pos,-1,1);
+
+			tmp=T2.qry(1,n,n-p[pos]+1,1);
+			T2.modify(1,n,tmp,n-pos+1,-1,1);
+
+			tmp=T2.find(1,n,n-pos+1,1);
+			int tmp2=T2.find_right(1,n,n-pos+1,1);
+			T2.active(1,n,tmp+1,n-pos,(tmp2<=n)?(n-p[n-tmp2+1]+1):INF,1);
+		}
+	}
+	printf("%d\n",ans);
 }
 int main(){
 	#ifdef LOCAL
 		assert(freopen("test.in","r",stdin));
 		assert(freopen("test.out","w",stdout));
 	#endif
-	ll T=read();
+	ll T=1;
 	// math_init();
 	while(T--) procedure();
 	return 0;
