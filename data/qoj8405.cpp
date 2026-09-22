a7f2409a352f563a4ce654a4974de7322b1146f6
CoderBreakPlus <breakplus@foxmail.com>
2026-09-23 07:53:59 +0800

    批量更新

---
+++ data/qoj8405.cpp
@@ -0,0 +1,61 @@
+// created time: 2026-09-23 07:39:35
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define fi first
+#define se second
+#define mkp make_pair
+#define pb emplace_back
+#define popcnt __builtin_popcountll
+const int mod = 998244353;
+inline ll read(){
+	ll x=0, f=1; int ch=getchar();
+	while(ch<'0' || ch>'9') { if(ch=='-') f=-1; ch=getchar(); }
+	while(ch>='0' && ch<='9') x=x*10+ch-'0', ch=getchar();
+	return x*f;
+}
+inline int lg2(int x){ return 31^__builtin_clz(x); }
+inline ll lg2(ll x){ return 63^__builtin_clzll(x); }
+template<typename T>inline void addmod(T &x){ if(x >= mod) x -= mod; }
+template<typename T>inline void chkmax(T &a,T b){ a=max(a,b); }
+template<typename T>inline void chkmin(T &a,T b){ a=min(a,b); }
+inline ll qpow(ll a,ll b){
+	ll ans=1, base=a;
+	while(b){
+		if(b&1) ans=ans*base%mod;
+		base=base*base%mod; b>>=1;
+	}
+	return ans;
+}
+inline ll INV(ll x){ return qpow(x, mod-2); }
+
+int n,a[300005],qz[300005];
+map<int,int>mp;
+
+void procedure(){
+	n=read();
+	for(int i=1;i<=n;i++) mp[read()]++;
+	for(auto [v,cnt]: mp){
+		for(int l=1,r;l<=cnt;l=r+1){
+			int d=cnt/l;
+			r=cnt/d;
+			qz[l]+=d,qz[r+1]-=d;
+		}
+	}
+	for(int i=1;i<=n;i++){
+		qz[i]+=qz[i-1];
+		printf("%d ",qz[i]*i);
+	}
+	puts("");
+}
+int main(){
+	#ifdef LOCAL
+		assert(freopen("test.in","r",stdin));
+		assert(freopen("test.out","w",stdout));
+	#endif
+	ll T=1;
+	// math_init();
+	while(T--) procedure();
+	return 0;
+}
